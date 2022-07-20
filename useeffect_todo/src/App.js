88d59320f4c84f43350bf7338398f01499be3a5b
Shivam Singh <shivam_fw12_029@masai.school>
2022-07-20 23:31:44 +0530

    useEffect

---
+++ useeffect_todo/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Learning useEffect</h2>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ useeffect_todo/src/components/Todo.jsx
@@ -0,0 +1,9 @@
+
+
+function Todo() {
+  return (
+    <div>Todo started hello</div>
+  )
+}
+
+export default Todo
